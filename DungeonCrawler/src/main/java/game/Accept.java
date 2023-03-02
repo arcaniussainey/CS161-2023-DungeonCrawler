cbd9abc9a0c70b25356165360b6832f9f820b8a6
brian <br.3cow@gmail.com>
2023-03-02 13:17:46 -0500

    Update Accept.java

--- DungeonCrawler/src/main/java/game/Accept.java
+++ DungeonCrawler/src/main/java/game/Accept.java
@@ -1,9 +1,6 @@
 package game;
 
 public class Accept extends Decision {
-	// This is an abstract parent class. 
-	// This class exists such that we can check if children derive from it, instead of checking both accept and reject. 
-	// It also means we can make a function return a Decision rather than specifically a reject or accept
 	public String message;
 	public Actor decided_actor; // this may be unneeded, it's added in case I decide to use it for the tilemap
 	
